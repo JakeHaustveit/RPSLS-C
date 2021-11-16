59fdbe9a3a5212171ec20ccc341e8f82f5111b52
Jacob Haustveit <jacobhaustveit@gmail.com>
2021-11-16 15:09:57 -0500

    Created Player class.

---
+++ RPSLS/RPSLS/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Player
+    {
+        public string name;
+        public string gestures;
+
+
+        public Player(string name)
+        {
+            this.name= name;
+            
+        }        
+
+        public void ChooseGesture()
+        {
+            string playerChoosenGesture = Console.ReadLine();
+
+            switch (playerChoosenGesture)
+            {
+                case ("Rock"):
+                    Console.WriteLine("Rock");
+                    this.gestures = "Rock";
+                    break;
+                case ("Paper"):
+                    Console.WriteLine("Paper");
+                    this.gestures = "Paper";
+                    break;
+                case ("Scissors"):
+                    Console.WriteLine("Scissors");
+                    this.gestures = "Scissors";
+                    break;
+                case ("Lizard"):
+                    Console.WriteLine("Lizard");
+                    this.gestures = "Lizard";
+                    break;
+                case ("Spock"):
+                    Console.WriteLine("Spock");
+                    this.gestures = "Spock";
+                    break;
+                default:
+                    Console.WriteLine("Not valid Gesture");
+                    break;
+                
+            }
+        }
+    }
+}
